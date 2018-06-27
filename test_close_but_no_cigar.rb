d974ae2dbf4b516c55cf9e3093d2c6b14c195bc7
William Mullins <twfdisturbed@hotmail.com>
2018-06-27 09:28:09 -0400

    Added a line to make the code fail

--- test_close_but_no_cigar.rb
+++ test_close_but_no_cigar.rb
@@ -1,10 +1,13 @@
 require "minitest/autorun"
-require_relative "close.rb"
+require_relative "close_but_no_cigar.rb"
 
 class TestCigar < Minitest::Test
 
     def test_assert_that_1_equals_1
         assert_equal(1, 1)
     end
-    
+    def test_if_function_has_string
+    	assert_equal(Integer, cigar("", "").class)
+    end
+        
 end
