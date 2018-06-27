01b3ce5b0b4c6f83352d64dd3bbece95f4b34705
William Mullins <twfdisturbed@hotmail.com>
2018-06-27 09:41:27 -0400

    testing if one number will return three

--- test_close_but_no_cigar.rb
+++ test_close_but_no_cigar.rb
@@ -12,4 +12,7 @@ class TestCigar < Minitest::Test
     def testing_if_one_match_returns_one
     	assert_equal(1, cigar("1234", "4567"))
     end        
+	def testing_if_one_match_returns_three
+    	assert_equal(3, cigar("1235", "0235"))
+    end
 end
